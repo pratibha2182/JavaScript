c7641939c252155bd8523daf0a5fb7b6796030ae
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 06:12:41 +0000

    PQ Q2

--- PQ/Question2.js
+++ PQ/Question2.js
@@ -1,1 +1,12 @@
-/**Store your friend’s marks and check if they are greater than 50. */
+/**Store your friend’s marks and check if they are greater than 50.  we will solve it by if else looping */
+
+const prompt = require("prompt-sync")();
+let marks = prompt("Enter Your Marks: ")
+
+if (marks > 50) {
+    console.log("You have score more then 50.");
+} else {
+    console.log("You have not score more then 50.")
+}
+console.log("Your marks is: " + marks);
+ 
