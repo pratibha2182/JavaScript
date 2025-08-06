b872fc1bb5fd75a91fd20f0eb8ec2d58babb2010
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 05:48:47 +0000

    Comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,16 @@
+/* Comparion Opertors it use to do comparison   betwen the value provided or varibles
+> = greater than
+< = less than 
+== = it will check only the values like 5 == "5" it will give true as it check the value
+=== = it will check the type of the value to like 5 === "5" it will give false because one is number and another is string
+!=  = it is same as == but for diffent value will give true
+*/
+
+// lets do with taking examples
+let a = 5
+let b = 3
+console.log("> "+(a > b));
+console.log("< "+(a < b));
+console.log("== "+(5 == "5"));
+console.log("=== "+(5 === "5"));
+console.log("!= "+(a != b));
