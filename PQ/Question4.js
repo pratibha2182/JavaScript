039ea4c9441fd5d4cb7e241a42fb27e266c47a9b
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 06:23:17 +0000

    PQ Q4

--- PQ/Question4.js
+++ PQ/Question4.js
@@ -1,1 +1,7 @@
 /**Find the remainder when 57 is divided by 6. */
+
+let a = 57
+let b = 6
+
+console.log("Remainer while dividing 57 with 6 is " + (a % b))
+console.log("Dividing 57 with 6 is " + (a / b))
