99e11ad2b5317e8df156bbec557237740729a360
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 05:39:14 +0000

    Arithmetic operators

---
+++ Basics/arithmetic.js
@@ -0,0 +1,16 @@
+/* Arithmetic Opertors it use use to do mathematics operations betwen the value provided or varibles
+Addition = "+" 
+Subtraction = "-" 
+Multiplication = "*"
+Division = "/"
+Module = "%" it will give you the remainer
+*/
+
+// lets do with taking examples
+let a = 10
+let b = 3
+console.log("Addition is " +(a+b));
+console.log("Subtraction is " + (a-b));
+console.log("Multiplication is " + (a*b));
+console.log("Divison is " + (a/b));
+console.log("Module is " + (a%b));
