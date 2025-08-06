fc0dee3816b0bd9b8ba22676234a82ec2ca5224f
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 05:30:14 +0000

    datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,43 @@
+/* there are some type of data used in javascript
+sting = "hello",'hello  (test should be inside qoutes)
+number = 3 , 1.2 (contains digits)
+boolean = true/false (contains true or false)
+null = null (empty)
+undefined = undefined (not gien any value but can be aign later)
+object = {name: "Pratibha"} (contain key value pairs in it)
+array = [1,2,3] (list of values)
+*/
+
+// let try making some examples 
+
+let name = "Pratibha"  // string
+let age = 22 // numbers
+let isStudent = false  //boolean
+let marks = null  // null
+let city // undefined
+let personality = {name: "Pratibha" , age: 22, gender: "female"}  //object
+let subject = ["c", "c++", "java", "python"]  //array
+
+
+console.log("Lets see the output of the varibles");
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(marks);
+console.log(city);
+console.log(personality);
+console.log(subject);
+
+console.log("\n \n Lets see the types of the varibles");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof marks);
+console.log(typeof city);
+console.log(typeof personality);
+console.log(typeof subject);
+
+
+
+
+
