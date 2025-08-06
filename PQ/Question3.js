e39709f6f2d925f658dbf6166d28ed92f29dc4ad
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 06:19:41 +0000

    PQ Q3

--- PQ/Question3.js
+++ PQ/Question3.js
@@ -1,1 +1,16 @@
 /**Swap two variables’ values using a third variable. */
+
+const prompt = require("prompt-sync")();
+
+let a = prompt("A: ")
+let b = prompt("B: ")
+
+let c = a
+a = b
+b = c
+
+console.log("A:",a);
+console.log("B:", b)
+
+
+// console.log (a,b)
