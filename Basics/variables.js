14dbdd145c028835eae9355c8c6f2c334c2c2ff6
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 05:12:46 +0000

    variable

---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+let name = "Pratibha"  // value of name can change later on. 
+const age = 22 // value can't be change anytime using const as it is constant value 
+var city = "Valsad" // value can be change any time but it is old method we can not use it in new javascpit becaoue of scope variable and function methods 
+
+//lets try chnage the values
+city = "Vadodara"
+name = "Kunjal"
+// age = 23 //Assignment to constant variable.
+
+//print the values 
+console.log(city);
+console.log(name);
+console.log(age);
