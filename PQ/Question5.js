fd73abbc5145477bb961a571b78f90686a93d950
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-06 06:25:01 +0000

    PQ Q5

--- PQ/Question5.js
+++ PQ/Question5.js
@@ -1,1 +1,5 @@
 /**Create a variable with const and try to change it — see what happens. */
+
+const name = "Pratibha"
+// tyr to change the const value
+// name = "Kunjal" //TypeError: Assignment to constant variable. at Object.<anonymous> (/workspaces/JavaScript/PQ/Question5.js:5:6)
