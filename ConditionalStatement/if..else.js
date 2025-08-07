c2a96862646e31e68393e42c7f45afca766fd43d
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-07 05:18:42 +0000

    if..else

---
+++ ConditionalStatement/if..else.js
@@ -0,0 +1,10 @@
+/**
+ it is used if you want to run true and false  condition at same time.
+ */
+
+ let score = 39
+ if (score >= 40){
+    console.log("Pass");
+ } else {
+    console.log("Fail");
+ }
