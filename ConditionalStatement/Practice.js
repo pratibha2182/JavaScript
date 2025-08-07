3f859fd790441c0cef6035a440ed5379d39cfdfc
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-07 05:47:08 +0000

    practice

--- ConditionalStatement/Practice.js
+++ ConditionalStatement/Practice.js
@@ -1,6 +1,17 @@
 //Q1: Pass or Fail
+
+/*
+const prompt = require("prompt-sync")()
+let num = prompt ("Enter a number:")
+if (num >= 50){
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+*/
+
 //Q2: Check Positive or Negative
-/** 
+/*
 const prompt = require("prompt-sync")()
 let num = prompt ("Enter a number:")
 if (num < 0){
@@ -24,6 +35,7 @@ if (num >= 100 && num <= 999){
 */
 
 //Q4:Voting Eligibility
+/*
 const prompt = require("prompt-sync")()
 let num = prompt ("Enter your age:")
 if (num >= 18){
@@ -32,7 +44,15 @@ if (num >= 18){
     console.log("You can't vote");
 }
 console.log("Your age is " + num);
-
-
+*/
 
 //Q5:Even or Odd Checker
+/*
+const prompt = require("prompt-sync")()
+let num = prompt ("Enter a number:")
+if (num % 2 == 0){
+    console.log("It is a even number");
+} else {
+    console.log("It is a odd number");
+}
+*/
