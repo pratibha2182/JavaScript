adb3784174be46e8a3e36ce86c257b1d9c31bebb
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-07 05:26:26 +0000

    nestedif

---
+++ ConditionalStatement/nestedif.js
@@ -0,0 +1,12 @@
+/** it contain if condition inside if statement */
+
+let age = 25;
+let hasID = false;
+
+if (age >= 18) {
+    if (hasID) {
+        console.log("You can enter.");
+    } else {
+        console.log("ID required.");
+    }
+}
